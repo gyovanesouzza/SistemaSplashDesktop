fd643a619984d721e9c29696166d084846a9f5ec
Gyovane Souza <gyovanesouzza@outlook.com.br>
2018-12-27 20:48:39 -0200

    Alterar

--- src/controller/FXMLAdicionarProfessorParteContatoController.java
+++ src/controller/FXMLAdicionarProfessorParteContatoController.java
@@ -6,11 +6,16 @@
 package controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -20,18 +25,47 @@ import javafx.scene.input.MouseEvent;
 public class FXMLAdicionarProfessorParteContatoController implements Initializable {
 
     @FXML
+    private AnchorPane acCorpo;
+
+    @FXML
+    private Pane pnTroca;
+
+    @FXML
+    private JFXTextField txtCep;
+
+    @FXML
+    private JFXTextField txtRua;
+
+    @FXML
+    private JFXTextField txtNumero;
+
+    @FXML
+    private JFXTextField txtComplemento;
+
+    @FXML
+    private JFXTextField txtTelefone;
+
+    @FXML
+    private JFXTextField txtCelular;
+
+    @FXML
+    private JFXTextField txtEmail;
+
+    @FXML
     private JFXButton btnProximo;
 
     @FXML
     private JFXButton btnCancelar;
 
     @FXML
-    void clickedProximo(MouseEvent event) {
-
+    void clickedProximo(MouseEvent event) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("/fxml/FXMLAdicionarProfessorParteAula.fxml"));
+        pnTroca.getChildren().setAll(pane);
     }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
